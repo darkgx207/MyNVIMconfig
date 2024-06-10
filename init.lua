c50603e019ad3cb44444af5a8d5807b4307c89bc
Guilherme Rocha <100701341+darkgx207@users.noreply.github.com>
2024-06-09 22:50:07 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,7 +7,12 @@ vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.termguicolors = true
 vim.o.clipboard = 'unnamedplus'
+vim.o.smartindent = true
+vim.g.colorschema = 'habamax'
+
+-- Global ----------------
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 vim.g.netrw_liststyle = 3
 
 --------- Key maps ---------------------------
@@ -15,8 +20,14 @@ local keymap = vim.api.nvim_set_keymap
 local opt = { noremap = true, silent = false }
 
 ---------------------------- Types -------------------------------------------
--- n -> NORMAL,  i -> INSERT, v -> VISUAL,  t -> TERM-MODE ,  c -> COMMAND  --
+-- n > NORMAL,  i > INSERT, v > VISUAL,  t > TERM-MODE ,  c > COMMAND  --
 
 keymap('n','<leader>q',':q!<CR>',opt)
 keymap('n','<C-s>',':w<CR>',opt)
 keymap('n','<leader>q',':q!<CR>',opt)
+keymap('n','<leader>t',':tabnew<CR>',opt)
+keymap('n', '<leader><C-r>', ':source %<CR>',opt)
+keymap('n', '<leader>e', ':E<CR>',opt)
+
+-- color scheme ( habamax, lunapeach )
+vim.cmd('colorscheme habamax')
